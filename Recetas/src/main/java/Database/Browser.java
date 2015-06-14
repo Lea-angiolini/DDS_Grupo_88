565ef069b3bd34028c24a2e39815b85b8921b029
Shatova Anastasia <ashatova1045@gmail.com>
2015-06-14 15:59:00 -0300

    Hice metodos para cargar el usuario en el browser y factory. Tambien cree la clase Usuario y Grupo

--- Recetas/src/main/java/Database/Browser.java
+++ Recetas/src/main/java/Database/Browser.java
@@ -1,20 +1,25 @@
 package Database;
 
+import java.io.ObjectInputStream.GetField;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 import org.apache.log4j.chainsaw.Main;
 
 import Database.Factory;
+import ObjetosDB.Usuario;
 
 public class Browser {
-	
-	public static void main(String[] args) {
-		if (Login("Emiliano", "13051990"))
-			System.out.println("Logueaste");
-		else
-			System.out.println("Gil");
-	}
 	
 	public static Boolean Login(String username, String password){
 		Factory f = new Factory();
 		return f.loguearUsuario(username, password);		
+	}
+	
+	public static Usuario cargarUsuario(String username){
+		Factory f = new Factory();
+		return f.cargarUsuario(username);
+		
 	}
 }
--- Recetas/src/main/java/Database/Factory.java
+++ Recetas/src/main/java/Database/Factory.java
@@ -2,6 +2,10 @@ package Database;
 
 import java.sql.*;
 
+import javax.swing.text.TableView;
+
+import ObjetosDB.Usuario;
+
 
 public class Factory {
 	
@@ -32,5 +36,31 @@ public class Factory {
 			
 		}
 		return resp;
+	}
+	
+	public Usuario cargarUsuario(String nombreUsuario){
+		ResultSet rs = null;
+		Usuario user = new Usuario();
+		try {
+			CallableStatement cmd = con.prepareCall("{call SP_CargarUsuario(?,?)}");
+			
+			cmd.setString("pUsuario", nombreUsuario);
+			cmd.registerOutParameter("usuarioACargar", Types.REF_CURSOR);
+			rs = cmd.executeQuery();
+			if(rs.next()){
+				user.setAltura(rs.getInt("altura"));
+				user.setNombre(rs.getString("nombre"));
+				user.setApellido(rs.getString("apellido"));
+				user.setComplexion(rs.getString("complexion"));
+				user.setRutina(rs.getString("rutina"));
+				user.setSexo(rs.getString("sexo").charAt(0));
+			}
+			
+		}
+		catch(SQLException ex){
+			
+		}
+		return user;
+		
 	}
 }
