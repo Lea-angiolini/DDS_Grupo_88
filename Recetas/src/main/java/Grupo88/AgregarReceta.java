396fa5b136f3e19fab4f84914b30ecb4aa8adf83
Lea-angiolini <angiolini.leandro@gmail.com>
2015-08-11 03:32:14 -0300

    empezando agregar receta

--- Recetas/src/main/java/Grupo88/AgregarReceta.java
+++ Recetas/src/main/java/Grupo88/AgregarReceta.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.prefs.BackingStoreException;
 
+import javax.swing.JOptionPane;
+
 import master.MasterPage;
 import objetosWicket.SesionUsuario;
 
 import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
@@ -20,9 +24,11 @@ import org.apache.wicket.markup.html.form.NumberTextField;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.form.SubmitLink;
+import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.AbstractItem;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -30,6 +36,7 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.eclipse.jetty.server.Server;
+import org.omg.CORBA.NVList;
 import org.apache.wicket.Session;
 
 import Grupo88.AltaUsuario.FrmAltaUsuario;
@@ -42,78 +49,70 @@ public class AgregarReceta extends MasterPage {
    
 	private SesionUsuario sesion = (SesionUsuario)getSession();
 	private Usuario user = sesion.getUsuario().getObject();
-	private FrmAgregarReceta frmAgregarReceta;
+	private Fragmento frmAgregarReceta;
+	private final RecetaU nuevareceta = new RecetaU(-1, "", "", "", "", "", 0);
 	
 	public AgregarReceta(){
 		super();
 		//getMenuPanel().setVisible(false);
 		
-		add(frmAgregarReceta = new FrmAgregarReceta("FrmAgregarReceta", user));
-		
-		frmAgregarReceta.add(new Link("cancelar"){
-			
-			public void onClick(){
-			setResponsePage(GestionarRecetas.class);
-			}
-		});
-		
+		add(frmAgregarReceta = new Fragmento("areaForms","fragmentoInicial",this,new FrmDatosReceta("frmDatosBasicos")));
+	}
+	
+	protected AgregarReceta pagina(){
+		return this;
+	}
+	
+	public class Fragmento extends Fragment{
+
+		public Fragmento(String id, String markupId, MarkupContainer markupProvider, Form form) {
+			super(id, markupId, markupProvider);
+			// TODO Auto-generated constructor stub
+			add(form);
+		}
 		
 	}
 	
-	public class FrmAgregarReceta extends Form {
-		 
+	
+	
+	public class FrmDatosReceta extends Form {
+		Model algo = new Model("");
 		
-		
-		private String nombreReceta;
-		private String ingrediente;
-		private String nuevoIngrediente;
-		private String condimento;
-		private String nuevoCondimento;
-		private String calorias;
-		private String tipoCondimento;
-		private DropDownChoice temporada;
-		private String caloriasTotales;
-		private List<String> clasificacion = Arrays.asList(new String[] {
-				"desayuno", "amuerzo", "merienda", "cena"});
-		private ArrayList<String> clasificacionesSeleccionadas = new ArrayList<String>();
-		private DropDownChoice grupoAlimenticio;
-
-		public FrmAgregarReceta(String id, Usuario user) {
-			super(id);			
-			setDefaultModel(new CompoundPropertyModel(this));
+		public FrmDatosReceta(String id) {
+			super(id);
+			// TODO Auto-generated constructor stub
 			
-		RecetaU nvaReceta = new RecetaU(0,"",user.getUsername(),"","","",-1);
-			
-		add(new TextField("nombreReceta"));
-		add(new TextField("ingrediente"));
-		add(new TextField("nuevoIngrediente"));
-		add(new TextField("condimento"));
-		add(new TextField("nuevoCondimento"));
-		add(new NumberTextField("calorias"));
-		add(new TextField("tipoCondimento"));
-		add(new RadioGroup("dificultad"));
-		add(new DropDownChoice("temporada"));
-		add(new NumberTextField("caloriasTotales"));
-		add(new CheckBoxMultipleChoice<String>("clasificacion", new Model(clasificacionesSeleccionadas),clasificacion));
-		// add(new RadioGroup("calificacion"));
-		add(new DropDownChoice("grupoAlimenticio"));
-		
-		/*
-		IModel<String> pasoActual = new Model<String>();
-		RepeatingView pasos = new RepeatingView("iterar");
-		AbstractItem item = new AbstractItem(pasos.newChildId());
-		item.add(new Label("numPaso", "Paso 1:"));
-		//item.add(new );
-				//new TextField<String>("areaTexto", pasoActual )); 
-		pasos.add(item);
-		add(pasos);*/
+		final TextField<String> nombre;
+		add(nombre = new TextField<String>("nombreReceta", new PropertyModel<String>(nuevareceta, "nombre")));
+		add(new TextArea<String>("descripcion", algo));
 		}
 		
 		@Override
 		protected void onSubmit() {
 		// Va a conectarse con BD y comprobar las validaciones
 		super.onSubmit();
+		pagina().addOrReplace(new Fragmento("areaForms","fragmentoPaso",pagina(), new FrmPaso("frmPasos")));
 		
+		
+		}
+	}
+	
+	public class FrmPaso extends Form {
+
+		public FrmPaso(String id) {
+			super(id);
+			// TODO Auto-generated constructor stub
+			
+	
+		}
+		
+		
+		@Override
+		protected void onSubmit() {
+		// Va a conectarse con BD y comprobar las validaciones
+		super.onSubmit();
+		JOptionPane.showMessageDialog(null, nuevareceta.getNombre());
+		//JOptionPane.showMessageDialog(null, algo.getObject());	
 		}
 	}
 }
