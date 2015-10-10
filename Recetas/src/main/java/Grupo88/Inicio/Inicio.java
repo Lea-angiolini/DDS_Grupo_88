8fa0fdeefdd0dd1185c88406eb99ca6246d7edef
Lea-angiolini <angiolini.leandro@gmail.com>
2015-10-09 22:25:43 -0300

    Arreglos xml

--- Recetas/src/main/java/Grupo88/Inicio/Inicio.java
+++ Recetas/src/main/java/Grupo88/Inicio/Inicio.java
@@ -1,21 +1,13 @@
 package Grupo88.Inicio;
 
-import java.util.List;
-
 import master.MasterPage;
 
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.hibernate.Hibernate;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
-import Database.HibernateUtil;
+
 import Grupo88.Componentes.ListaDeRecetas;
-import ObjetosDB.Ingredientes;
-import ObjetosDB.Receta_Ingrediente;
+
 
 public class Inicio extends MasterPage {
 
@@ -29,7 +21,6 @@ public class Inicio extends MasterPage {
 	public Inicio(){
 		super();
 		//getMenuPanel().setVisible(false);
-		
 		
 		add(frmInicio = new FrmInicio("FrmInicio"));
 	}
