752d4c8c5e757d43b4e599297d0097bee136fae8
Shatova Anastasia <ashatova1045@gmail.com>
2015-10-28 15:53:47 -0300

    se me olvido subir esto perdon

---
+++ Recetas/src/main/java/Database/DAOGruposAlimenticios.java
@@ -0,0 +1,14 @@
+package Database;
+
+import ObjetosDB.GruposAlimenticios;
+
+public class DAOGruposAlimenticios extends DAOGenerico<GruposAlimenticios, Integer>{
+	
+	public DAOGruposAlimenticios() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+}
+
+//se me olvido subir esto, perdon
